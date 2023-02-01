00aaca161150cdc564466f0c6a801db3501d876f
Akumsk <36819170+Akumsk@users.noreply.github.com>
2023-02-01 13:15:29 +0800

    Delete Makku directory

--- Makku/users/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path, include
-from . import views as views_user
-
-urlpatterns = [
-    path('accounts/', include('django.contrib.auth.urls')),
-#    path('card/<str:user_id>', views_user.user_card, name='user-name'),
-
-]
